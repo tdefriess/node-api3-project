748c0f0fb9f12b821d8f0315255e40f91aa11505
Tyler de Friess <tyler.defriess@gmail.com>
2020-03-11 18:25:38 -0400

    Project setup, custom logger middleware added.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code away!
+const server = require('./server.js');
+
+const PORT = 5000;
+server.listen(PORT, () => {
+    console.log(`\n* ServerRunning on http://localhost:${PORT} *\n`)
+});
--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express');
 
 const server = express();
 
+server.use(logger);
+
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.originalUrl} at ${new Date().toISOString()}`)
+
+  next();
+}
 
 module.exports = server;
