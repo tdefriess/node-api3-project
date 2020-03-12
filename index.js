de13154695429c5ff5434bc210bc5950e516107d
Tyler de Friess <tyler.defriess@gmail.com>
2020-03-12 14:02:33 -0400

    Updated port to be dynamic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server.js');
 
-const PORT = 5000;
-server.listen(PORT, () => {
-    console.log(`\n* ServerRunning on http://localhost:${PORT} *\n`)
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log(`\n* ServerRunning on http://localhost:${port} *\n`)
 });
